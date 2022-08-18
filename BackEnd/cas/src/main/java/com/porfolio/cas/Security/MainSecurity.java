086f463994bd64b64e84f7243ada8db7c1fd845d
AsmaTycoon <cristiansalzgeber@hotmail.com>
2022-08-17 21:29:44 -0300

    tratando de reparar backend

--- BackEnd/cas/src/main/java/com/porfolio/cas/Security/MainSecurity.java
+++ BackEnd/cas/src/main/java/com/porfolio/cas/Security/MainSecurity.java
@@ -38,7 +38,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests().antMatchers("/auth/**").permitAll().anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().and().csrf().disable().authorizeRequests().antMatchers("**").permitAll().anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
--- BackEnd/cas/src/main/java/com/porfolio/cas/Security/jwt/JwtTokenFilter.java
+++ BackEnd/cas/src/main/java/com/porfolio/cas/Security/jwt/JwtTokenFilter.java
@@ -42,7 +42,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private String getToken(HttpServletRequest request){
         String header = request.getHeader("Authorization");
         if(header != null && header.startsWith("Bearer"))
-            return header.replace("Bearer", "");
+            return header.replace("Bearer","");
         return null;
     }
 }
